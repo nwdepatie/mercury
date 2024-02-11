224f6647339b3b0fcfe6b2e0d73ee56fadbbd7d6
nwdepatie <nwdepatie@gmail.com>
2024-02-10 22:32:12 -0500

    Adding in some more MIO stuff

--- mercury-app/pmod_pwm/pmod_pwm.c
+++ mercury-app/pmod_pwm/pmod_pwm.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * https://www.realdigital.org/doc/4b3000d07f821a225e90486b4ea33815 Q
  */
 
@@ -15,67 +15,89 @@
 #define MIO_PIN_18 0xF8000748
 #define MIO_PIN_50 0xF80007C8
 #define MIO_PIN_51 0xF80007CC
+#define GPIO_DIRM_0 0xE000A204 // Direction mode bank 0
+#define GPIO_OUTE_0 0xE000A208 // Output enable bank 0
+#define GPIO_DIRM_1 0xE000A244 // Direction mode bank 1
 
 // register offsets
 #define REG_OFFSET(REG, OFF) ((REG)+(OFF))
 
-// map GPIO peripheral; base address will be mapped to gpioregs pointer
-static int map_gpio(unsigned int** gpioregs, unsigned int gpio_addr)
+/// stop infinite loop flag
+static unsigned char stopLoop = 0;
+
+/* @brief Handle signals
+	 @param num Signal Number */
+static void signal_handler(int num)
 {
-  int fd = 0;
+	stopLoop = 1;
+}
 
-  /* Cleanse input */
-  if (!gpioregs || !gpio_addr) {
-    // invalid pointers
-    return 1;
-  }
+// map GPIO peripheral; base address will be mapped to gpioregs pointer
+static int map_peripheral(unsigned int** peripheral, unsigned int peripheral_addr)
+{
+	int fd = 0;
 
-  /* Open /dev/mem to retrieve memory */
-  fd = open("/dev/mem", O_RDWR);
-  if (fd < 0) {
-    // can't open file
-    return fd;
-  }
+	/* Cleanse input */
+	if (!peripheral || !peripheral_addr) {
+		// invalid pointers
+		return 1;
+	}
 
-  /* mmap the GPIO register */
-  *gpioregs = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_SHARED, fd, gpio_addr);
-  //if (PTR_ERR(*gpioregs)) {
-  //  return PTR_ERR(*gpioregs);
-  //}
+	/* Open /dev/mem to retrieve memory */
+	fd = open("/dev/mem", O_RDWR);
+	if (fd < 0) {
+		// can't open file
+		return fd;
+	}
 
-  close(fd);
+	/* mmap the GPIO register */
+	*peripheral = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_SHARED, fd, peripheral_addr);
+	//if (PTR_ERR(*gpioregs)) {
+	//  return PTR_ERR(*gpioregs);
+	//}
 
-  // success
-  return 0;
+	close(fd);
+
+	// success
+	return 0;
 }
 
 int main(int argc, char** argv)
 {
-  unsigned int *gpioregs;
+	unsigned int *gpiocfg;
 
-  // try to setup LEDs
-  if (map_gpio(&gpioregs, LED_GPIO_ADDR)) {
-    // failed to setup
-    return 1;
-  }
+	float period      = 1; /* ms */
+	float duty_cycle  = 50; /* percent */
 
-  // reset control register
-  *REG_OFFSET(gpioregs, 0) = 0x00;
+	// try to setup LEDs
+	if (map_peripheral(&gpiocfg, GPIO_DIRM_0)) {
+		// failed to setup
+		return 1;
+	}
 
-    char input[256];
+	char input[256];
 
-    // Get Temperature in Fahrenheit from user
-    printf("Enter a decimal number N to count up to: ");
-    fgets(input, sizeof(input), stdin);
-    int number = atoi(input);
-    int counter = 0;
+	// Get Temperature in Fahrenheit from user
+	printf("Enter a decimal number N to count up to: ");
+	fgets(input, sizeof(input), stdin);
+	int number = atoi(input);
+	int counter = 0;
 
-  while (counter <= number) {
-      //do something here!
-      *REG_OFFSET(gpioregs, 0) = counter;
-      usleep(1000000);
-      counter++;
-  }
+	while (!stopLoop) {
+		err = axigpio_single_set(gpio, 0, CHANNEL_NUM);
+		if (err)
+			return err;
 
-  return 0;
+		usleep(MS_TO_US(period * duty_cycle / 100));
+
+		err = axigpio_single_clear(gpio, 0, CHANNEL_NUM);
+		if (err)
+			return err;
+
+		usleep(MS_TO_US(period * (1 - (duty_cycle / 100))));
+	}
+
+	//de-initialize peripherals here
+	axigpio_free(gpio);
+	return 0;
 }
