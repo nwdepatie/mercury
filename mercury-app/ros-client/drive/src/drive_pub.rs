22cdf0975fde9d1103b799dda210496403da44ef
nwdepatie <nwdepatie@gmail.com>
2024-02-17 09:59:04 -0500

    Consolidating drive ROS nodes into 1 cargo environment

--- mercury-app/ros-client/drive/src/drive_pub.rs
+++ mercury-app/ros-client/drive/src/drive_pub.rs
@@ -1,32 +1,17 @@
-/* Publisher for Emitting Info Relevant to Chassis Drive Motors */
-
-struct MotorControlNode {
-    wheel_base: f64,
-    wheel_radius: f64,
-    max_velocity_meters_per_second: f64,
-    max_angular_velocity_rad_per_second: f64,
-    velocities: RwLock<(f64, f64)>,
-}
-
 fn main() {
     // Initialize node
-    rosrust::init("drive_pub");
+    rosrust::init("talker");
 
     // Create publisher
-    let fault_pub = rosrust::publish("/drive/faults", 2).unwrap();
-    let enc_vel_pub = rosrust::publish("/drive/encoder/ticks", 2).unwrap();
-    let enc_ticks_pub = rosrust::publish("chatter", 2).unwrap();
-
-    fault_pub.wait_for_subscribers(None).unwrap();
-    enc_vel_pub.wait_for_subscribers(None).unwrap();
-    enc_ticks_pub.wait_for_subscribers(None).unwrap();
+    let chatter_pub = rosrust::publish("chatter", 2).unwrap();
+    chatter_pub.wait_for_subscribers(None).unwrap();
 
     let log_names = rosrust::param("~log_names").unwrap().get().unwrap_or(false);
 
     let mut count = 0;
 
     // Create object that maintains 10Hz between sleep requests
-    let rate = rosrust::rate(100.0);
+    let rate = rosrust::rate(1000.0);
 
     // Breaks when a shutdown signal is sent
     while rosrust::is_ok() {
