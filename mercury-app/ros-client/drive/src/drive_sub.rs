24ca68b4e415596ccc462177d173c9c91f94ed69
nwdepatie <nwdepatie@gmail.com>
2024-02-17 12:37:28 -0500

    Separating motor and drive characteristics from controller

--- mercury-app/ros-client/drive/src/drive_sub.rs
+++ mercury-app/ros-client/drive/src/drive_sub.rs
@@ -3,17 +3,18 @@
 use rosrust;
 use rosrust_msg::geometry_msgs::Twist;
 use std::sync::RwLock;
+pub mod motor_model;
+use self::motor_model::DriveMotorModel;
 
-struct DriveController {
-	wheel_base: f64,
-	wheel_radius: f64,
+pub struct DriveController {
+	model : DriveMotorModel,
 	max_velocity_meters_per_second: f64,
 	max_angular_velocity_rad_per_second: f64,
 	velocities: RwLock<(f64, f64)>, /* Left and right velocities */
 }
 
 impl DriveController {
-	fn new() -> Self {
+	pub fn new() -> Self {
 		// Parameters
 		let wheel_base = rosrust::param("~wheel_base").unwrap().get().unwrap_or(0.2);
 		let wheel_radius = rosrust::param("~wheel_radius").unwrap().get().unwrap_or(0.095);
@@ -21,8 +22,7 @@ impl DriveController {
 		let max_angular_velocity_rad_per_second = rosrust::param("~max_angular_velocity_rad_per_second").unwrap().get().unwrap_or(6.0);
 
 		DriveController {
-			wheel_base,
-			wheel_radius,
+			model : DriveMotorModel::new(wheel_base, wheel_radius),
 			max_velocity_meters_per_second,
 			max_angular_velocity_rad_per_second,
 			velocities: RwLock::new((0.0, 0.0)),
@@ -47,12 +47,12 @@ impl DriveController {
 		rosrust::ros_info!("Left Vel/Command: {}, Right Vel/Command: {}", velocities.0, velocities.1);
 	}
 
-	fn command_callback(&self, data: Twist) {
+	pub fn command_callback(&self, data: Twist) {
 		let linear_velocity = self.clamp_linear_speed(data.linear.x);
 		let angular_velocity = self.clamp_angular_speed(data.angular.z);
 
-		let left_velocity = linear_velocity - (angular_velocity * self.wheel_base / 2.0);
-		let right_velocity = linear_velocity + (angular_velocity * self.wheel_base / 2.0);
+		let left_velocity = linear_velocity - (angular_velocity * self.model.wheel_base() / 2.0);
+		let right_velocity = linear_velocity + (angular_velocity * self.model.wheel_base() / 2.0);
 
 		// Store the velocities safely
 		let mut velocities = self.velocities.write().unwrap();
