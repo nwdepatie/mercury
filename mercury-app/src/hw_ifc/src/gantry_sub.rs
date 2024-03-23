1c0dcfcfedc50e2c766421c66328dfc46966355b
nwdepatie <nwdepatie@gmail.com>
2024-03-23 18:19:02 -0400

    Adding in services

--- mercury-app/src/hw_ifc/src/gantry_sub.rs
+++ mercury-app/src/hw_ifc/src/gantry_sub.rs
@@ -4,9 +4,10 @@ pub mod gantry_model;
 use self::gantry_model::{
     GantryAxes, GantryModel, GantryPosition, StepperCtrlCmd, StepperCtrlCmdGroup,
 };
+use std::sync::Arc;
 
 pub mod msg {
-    rosrust::rosmsg_include!(hw_srv/calibrate);
+    rosrust::rosmsg_include!(hw_srv / movegantry, hw_srv / calibrategantry);
 }
 
 pub struct GantryController {
@@ -16,41 +17,44 @@ pub struct GantryController {
 impl GantryController {
     pub fn new() -> Self {
         GantryController {
-			model: GantryModel::new(),
-		}
+            model: GantryModel::new(),
+        }
     }
 
-    pub fn command_callback(&self, data: rosrust_msg::hw_srv::calibrate) {}
+    pub fn move_callback(&self, data: rosrust_msg::hw_srv::movegantryReq) {}
+
+    pub fn calibrate_callback(&self) {}
 }
 
 fn main() {
     /* Initialize ROS node */
     rosrust::init("gantry_sub");
 
-    let gantry_ctrl = GantryController::new();
+    let gantry_ctrl = Arc::new(GantryController::new());
 
-    /*
-     * Create subscriber
-     */
-    //let _subscriber_info = rosrust::subscribe_with_ids(
-    //    "/gantry/pose/goal",
-    //    2,
-    //    move |v: rosrust_msg::hw_srv::calibrate, _caller_id: &str| {
-    //        gantry_ctrl.command_callback(v);
-    //    },
-    //)
-    //.unwrap();
+    /* Create subscriber */
+    let gantry_ctrl_clone = gantry_ctrl.clone();
+    let service_move = rosrust::service::<rosrust_msg::hw_srv::movegantry, _>(
+        "/gantry/pose/goal",
+        move |coords: rosrust_msg::hw_srv::movegantryReq| {
+            gantry_ctrl_clone.move_callback(coords);
 
-    /**
-     * Create service
-     */
-    //let _service_raii =
-    //    rosrust::service::<rosrust_msg::hw_srv::calibrate, _>("/services/gantry/calibrate", move |req| {
-    //        gantry_ctrl.command_callback(req);
-//
-    //        Ok(rosrust_msg::hw_srv::calibrate { })
-    //    })
-    //    .unwrap();
+            Ok(rosrust_msg::hw_srv::movegantryRes { status: true })
+        },
+    )
+    .unwrap();
+
+    /* Create service */
+    let gantry_ctrl_clone = gantry_ctrl.clone();
+    let service_calibrate = rosrust::service::<rosrust_msg::hw_srv::calibrategantry, _>(
+        "/services/gantry/calibrate",
+        move |_| {
+            gantry_ctrl_clone.calibrate_callback();
+
+            Ok(rosrust_msg::hw_srv::calibrategantryRes { status: true })
+        },
+    )
+    .unwrap();
 
     while rosrust::is_ok() {
         /* Spin forever, we only execute things on callbacks from here */
